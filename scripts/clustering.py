9163a0ccf80511cbeac0238aa09f20d009f91952
marianaiv <marianaalbrnz@gmail.com>
2021-08-31 21:49:11 -0400

    funcion para leer ascii como dataframe

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -3,7 +3,8 @@ from subestructura import deltaR, tau21
 import pandas as pd
 import numpy as np
 import os.path                          # Manejo de directorios
-from os import path                                         # Manejo de paths
+from os import path                     # Manejo de paths
+from io import StringIO
 
 def generador(filename, chunksize=512,total_size=1100000):
     """Genera un dataframe de pandas utilizando parte de los datos de entrada,
@@ -37,6 +38,35 @@ def generador(filename, chunksize=512,total_size=1100000):
         m+=1
         if (m+1)*chunksize > total_size:
             m=0
+
+def leer_labels(path_label, column_name=2100):
+    """Lee el archivo ASCII que contiene los labels de los eventos: si son señal o fondo.
+
+    Si el argumento `column_name` no es pasado, se utiliza un nombre predeterminados.
+
+    Parametros
+    ----------
+    path_label : str
+        Dirección del archivo ASCII con los labels
+
+    column_name : str
+        Nombre de la columna (por defecto es 2100)
+
+    Devuelve
+    ------
+    Pandas Dataframe
+        Un dataframe con parte los labels de los eventos
+    """
+    # leemos el archivo con el key. Es un archivo ASCII donde cada linea corresponde a la información de señal o fondo
+    with open(path_label, 'r') as f:
+        data = f.read()
+    # Lo convertimos en dataframe
+    df = pd.read_csv(StringIO(data), header=None)
+    # Le cambiamos el nombre a la columna para que al concatenar con los datos no tenga el mismo nombre que otra columna
+    df = df.rename(columns={0: column_name})
+    
+    return df
+
 
 def jets(evento, R = 1.0, p = -1, ptmin=20): 
 
