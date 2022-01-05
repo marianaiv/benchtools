a48a9bf7430756e32aecd866730fbe0bd8e511b4
marianaiv <marianaalbrnz@gmail.com>
2022-01-04 22:54:58 -0400

    first tests for benchtools package

---
+++ tests/test_datatools.py
@@ -0,0 +1,52 @@
+"""
+Test datatools
+"""
+import pandas as pd
+import os
+
+from benchtools.src.clustering import build_features
+from benchtools.src.datatools import read_multifiles, generator, ascii_column, separate_data, save_df
+
+
+def test_read_multifiles():
+    
+    build_features(path_data="../data/events_anomalydetection_tiny.h5",nbatch=2, outname='test_file', chunksize=100)
+    # test lenght:
+    df = read_multifiles(filename='test_file', nbatch=2, outdir='../data')
+    assert df.shape[0]==100*2
+
+    # check for duplicated indexes
+    assert df.index.duplicated().all()==False
+
+#def test_generator():
+
+def test_ascii_column():
+    df = ascii_column(path_label='../data/test_file.masterkey')
+    # checking the length
+    assert df.shape == (40,1)
+    # checking that it only has 0 and 1
+    df[2100].isin([0,1]).all() == True
+
+def test_separate_data():
+    df = read_multifiles(filename='test_file', nbatch=2, outdir='../data')
+    X, y = separate_data(df)
+    
+    # check y only has 0 or 1
+    assert y.isin([0,1]).all() == True
+
+    # check columns of X
+    assert X.columns.tolist() == df.drop('label', axis=1).columns.tolist()
+
+    # check the rows of both df is the same
+    assert X.shape[0]==y.shape[0]
+
+def test_save_df():
+    df = read_multifiles(filename='test_file', nbatch=2, outdir='../data')
+    save_df(outname='test', outdir='../data/test_directory', df=df)
+    # check for creation of the directory an file
+    assert os.path.exists('../data/test_directory/test.csv')==True
+
+    # removing the files created for the test
+    if os.path.exists('../data/test_directory'):
+        os.remove('../data/test_directory/test.csv')
+        os.rmdir('../data/test_directory')
