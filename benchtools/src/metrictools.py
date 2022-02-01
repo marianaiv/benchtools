fixed random classification line in precision-recall plot function

benchtools/src/metrictools.py
@@ -385,8 +385,11 @@ def precision_recall_plot(names, labels, probs, colors=None):
             plt.plot(recall, precision, color=color, lw=2,
                     label='{} AP={:.3f}'.format(name,ap_score))
 
-    # Plotting the line for a random classifier
-    plt.plot([0, 0], [0, 0], color='navy', lw=1, linestyle='--', label='Random classification')
+        _, counts = np.unique(labels, return_counts=True)
+        ratio = counts[1]/counts[0]
+        print(ratio)
+        # Plotting the line for a random classifier
+        plt.axhline(y=ratio, color='navy', lw=1, linestyle='--', label='Random classification')
     
     # Adding information to the plot
     plt.legend(loc="lower right")