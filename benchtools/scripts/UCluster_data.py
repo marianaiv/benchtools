bdf927991d48145467d418a3677b66ecd3f480e0
marianaiv <marianaalbrnz@gmail.com>
2022-03-08 01:02:25 -0400

    changing paths

--- benchtools/scripts/UCluster_data.py
+++ benchtools/scripts/UCluster_data.py
@@ -2,6 +2,7 @@
 Script to convert the results of UCluster into the format needed for the benchmarking with run.py
 '''
 
+import os
 import h5py
 import pickle
 import argparse
@@ -11,9 +12,9 @@ from scripts.run import classifier
 
 # DEFAULT SETTINGS
 parser = argparse.ArgumentParser()
-parser.add_argument('--path', type=str, help='Path for the file with the data to transform')
+parser.add_argument('--path', type=str, default='../../data', help='Path for the file with the data to transform [default: ../data]')
 parser.add_argument('--box', type=int, default=1, help='Black Box number, ignored if RD dataset [default: 1]')
-parser.add_argument('--RD',  type=bool, default=True ,help='If is from the RD dataser [default: False')
+parser.add_argument('--RD',  default=False, action='store_true',help='Use RD data set [default: False]')
 
 flags = parser.parse_args()
 
@@ -21,11 +22,17 @@ DATA = flags.path  #rd_dist_full.h5
 NBOX = flags.box
 RD = flags.RD
 
+if RD:
+    sample = 'full_data_full_training_RD.h5'
+
+else:
+    sample = 'full_data_full_training_BB{}.h5'.format(NBOX)
+
 # Setting the variables for the classfier object
 name = 'UCluster'
-score = np.array(h5py.File(DATA, 'r')['distances'])
-pred = np.array(h5py.File(DATA, 'r')['pid'])
-label = np.array(h5py.File(DATA, 'r')['label'])
+score = np.array(h5py.File(os.path.join(DATA, sample), 'r')['distances'])
+pred = np.array(h5py.File(os.path.join(DATA, sample), 'r')['pid'])
+label = np.array(h5py.File(os.path.join(DATA, sample), 'r')['label'])
 
 # To calculate the score for a clustering algorithm
 norm = np.linalg.norm(score[:,1])
@@ -34,7 +41,7 @@ norm = np.linalg.norm(score[:,1])
 clf = classifier(name, 1-score[:,1]/norm, pred, label)
 
 # Saving
-if RD is True: filename = '{}_{}.sav'.format(name,RD)
+if RD is True: filename = '{}_RD.sav'.format(name)
 else: filename = '{}_BB{}.sav'.format(name,NBOX)
 
-pickle.dump(clf, open('../../data/models/{}'.format(filename), 'wb'))
+pickle.dump(clf, open('../../data/{}'.format(filename), 'wb'))
