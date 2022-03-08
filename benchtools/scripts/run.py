69ea18fc514561183aeee42df9318415a1ab9726
marianaiv <marianaalbrnz@gmail.com>
2022-03-08 18:01:30 -0400

    typo fix

--- benchtools/scripts/run.py
+++ benchtools/scripts/run.py
@@ -322,7 +322,7 @@ def main():
         # Dropping the mass to make the classification model-fre
         X_test.drop(['m_j1', 'm_j2', 'm_jj'], axis=1, inplace=True)
 
-    if TRAINING is True:
+    if TRAINING:
         # Scalers and classifiers
         classifiers = [(MinMaxScaler(feature_range=(-1,1)), TensorflowClassifier(input_shape = [X_train.shape[1]])),
                         (StandardScaler(), RandomForestClassifier(random_state=1)),
