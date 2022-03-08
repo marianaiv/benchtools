2f164657cc9c4ead4921bbff8884834dac018ec1
marianaiv <marianaalbrnz@gmail.com>
2022-03-07 23:51:50 -0400

    importing class

--- benchtools/scripts/GAN-AE_data.py
+++ benchtools/scripts/GAN-AE_data.py
@@ -7,7 +7,7 @@ import pickle
 import argparse
 import numpy as np
 import pandas as pd
-from scripts.run import classifier
+from run import classifier
 from benchtools.src.metrictools import optimal_threshold
 
 # DEFAULT SETTINGS
