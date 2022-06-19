23c9811f3433b99333fec795e39119e3db084f3e
marianaiv <marianaalbrnz@gmail.com>
2022-06-19 19:30:27 -0400

    extending readme file

--- setup.py
+++ setup.py
@@ -19,6 +19,11 @@ setuptools.setup(
     packages=["benchtools"
             , "benchtools.src"
            , "benchtools.scripts"
-]
+    ],
+    
+    entry_points={"console_scripts": [
+        "benchtools_run=benchtools.scripts.run:main"
+
+        ]},
 
 )
